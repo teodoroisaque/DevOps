62a814b42a805407333c8a09aec9eaf2960995b3
Isaque Teodoro <isaque@quiteja.com.br>
2021-06-02 16:42:39 -0300

    ajustando indentação

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,21 +1,21 @@
 module "vpc" {
-    source                  = "terraform-aws-modules/vpc/aws"
-    version                 = "2.77.0"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.77.0"
 
-    name                    = "esx-${var.infra_env}-vpc"
-    cidr                    = var.vpc_cidr
-    azs                     = var.azs
+  name = "esx-${var.infra_env}-vpc"
+  cidr = var.vpc_cidr
+  azs  = var.azs
 
-    enable_nat_gateway      = true
-    single_nat_gateway      = true
-    one_nat_gateway_per_az  = false
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
 
-    private_subnets         = var.private_subnets
-    public_subnets          = var.public_subnets
-    
-    tags = {
-        Name                = "esx-${var.infra_env}-vpc"
-        Project             = "esx-teste"
-        Environment         = "${var.infra_env}"
-    }
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  tags = {
+    Name        = "esx-${var.infra_env}-vpc"
+    Project     = "esx-teste"
+    Environment = "${var.infra_env}"
+  }
 }
--- terraform/modules/vpc/sg.tf
+++ terraform/modules/vpc/sg.tf
@@ -1,49 +1,49 @@
 resource "aws_security_group" "public" {
-  name              = "esx-${var.infra_env}-public-sg"
-  description       = "Internet Access Public"
-  vpc_id            = module.vpc.vpc_id
+  name        = "esx-${var.infra_env}-public-sg"
+  description = "Internet Access Public"
+  vpc_id      = module.vpc.vpc_id
 
   tags = {
-      Name          = "esx-${var.infra_env}-public-sg"
-      Role          = "public"
-      Projetct      = "esx-teste"
-      Environment   = "var.infra_env"
+    Name        = "esx-${var.infra_env}-public-sg"
+    Role        = "public"
+    Projetct    = "esx-teste"
+    Environment = "var.infra_env"
   }
 }
 
 resource "aws_security_group_rule" "public_out" {
-    type            = "egress"
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    
-    security_group_id = aws_security_group.public.id
-  
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.public.id
+
 }
 
 resource "aws_security_group_rule" "public_in_ssh" {
-    type            = "ingress"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
-    #Liberado para 0.0.0.0/0 para efeitos de teste, 
-    # extremamente importante para segurança usar um /32,
-    # ou vpn site to site para ssh.
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  #Liberado para 0.0.0.0/0 para efeitos de teste, 
+  # extremamente importante para segurança usar um /32,
+  # ou vpn site to site para ssh.
 
-    security_group_id = aws_security_group.public.id
-  
+  security_group_id = aws_security_group.public.id
+
 }
 
 resource "aws_security_group_rule" "private_in" {
-    type            = "ingress"
-    from_port       = 0
-    to_port         = 65535
-    protocol        = "-1"
-    cidr_blocks     = [module.vpc.vpc_cidr_block]
+  type        = "ingress"
+  from_port   = 0
+  to_port     = 65535
+  protocol    = "-1"
+  cidr_blocks = [module.vpc.vpc_cidr_block]
 
-    security_group_id = aws_security_group.public.id
-  
+  security_group_id = aws_security_group.public.id
+
 }
 
